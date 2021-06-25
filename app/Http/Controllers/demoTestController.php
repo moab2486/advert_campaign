e1efafee10791ffdfd536c4d2c7ec39fe085267c
moab2486 <abdulrasakabdulkadir@yahoo.com>
2021-06-25 06:34:13 +0100

    books crud

--- app/Http/Controllers/demoTestController.php
+++ app/Http/Controllers/demoTestController.php
@@ -2,120 +2,159 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BookResource;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use App\Models\Book;
 
 class demoTestController extends Controller
 {
     protected $url = "https://www.anapioficeandfire.com/api/books";
-        
 
+        
     protected function externalEndPoint($url)
     {
         $client = new Client();
 
-        $headers = [
-            'api-key' => 'k3Hy5qr73QhXrmHLXhpEh6CQ'
-        ];
-
         $response = $client->request('GET', $url, [
-            'headers' => $headers,
             'verify'  => false,
         ]);
 
         $responseBody = json_decode($response->getBody());
 
         $output = [];
-
-        if(count($output) < 1){
-            return response($output);
-        }
-
         foreach($responseBody as $d => $a){
             $output[] = [
-                        'name' => $a->name,
-                        'isbn' => $a->isbn,
-                        'authors' => $a->authors,
-                        'numberOfPages' => $a->numberOfPages,
-                        'publisher' => $a->publisher,
-                        'country' => $a->country,
-                        'mediaType' => $a->mediaType,
-                        'released' => $a->released
-                    ];
+                            'name' => $a->name,
+                            'isbn' => $a->isbn,
+                            'authors' => $a->authors,
+                            'number_of_pages' => $a->numberOfPages,
+                            'publisher' => $a->publisher,
+                            'country' => $a->country,
+                            'released_date' => $a->released
+                        ];
         };
-        
+
         return response($output);
     }
 
     public function searchBookWithParam(Request $request){
-        $url = $this.url();
+        $queryParam = $request->query('name');
 
-        $output = $this->externalEndPoint($url);
-
-        return response([
-            "status" => 201,
-            "message" => "External books searched",
-            "data" => $output
-        ]);
-
-        return response([
-            "status" => 404,
-            "message" => "Invalid request",
-        ]);
-    }
-
-    public function postBook(Request $request){
-        $url = "https://www.anapioficeandfire.com/api/";
-        return response([
-            "status" => 201,
-            "message" => "External books searched",
-            "data" => []
-        ]);
-
-        return response([
-            "status" => 201,
-            "message" => "Invalid request",
-        ]);
-    }
-
-    public function getBook(Request $request){
         $url = $this->url;
 
         $res = $this->externalEndPoint($url)->original;
+        
+        $key = array_search($queryParam, array_column($res, 'name'));
+
+        $data = $res[$key];
+
+        if($key){
+            return response([
+                "status" => 201,
+                "message" => "External books searched",
+                "data" => $data
+            ]);
+        }
+        
+        return response([
+            "status" => 204,
+            "message" => "Invalid request",
+        ]);
+    }
+
+    public function createBook(Request $request){
+        $book = new Book;
+
+        $book->name = $request->input('name');
+        $book->isbn = $request->input('isbn'); 
+        $book->authors = $request->input('authors');
+        $book->country = $request->input('country');
+        $book->number_of_pages = $request->input('number_of_pages');
+        $book->publisher = $request->input('publisher');
+        $book->release_date = $request->input('release_date');  
+        
+
+        if ($book->save()) {
+            $book->message = "Books added";
+            $book->status_code = 201;
+            return new BookResource($book);
+        } 
+    }
+
+    public function getBook(Request $request){
+        $get_books = Book::get();
+        
+        $output = [];
+        foreach($get_books as $d => $a){
+            $output[] = [
+                            'id' => $a->id,
+                            'name' => $a->name,
+                            'isbn' => $a->isbn,
+                            'authors' => $a->authors,
+                            'number_of_pages' => $a->numberOfPages,
+                            'publisher' => $a->publisher,
+                            'country' => $a->country,
+                            'released_date' => $a->released
+                        ];
+        };
 
         return response([
             "status" => 201,
-            "message" => "External books searched",
-            "data" => $res
+            "message" => "Book retrieved",
+            "data" => $output
         ]);
     }
 
     public function updateBook(Request $request, $id){
-        $url = "https://www.anapioficeandfire.com/api/";
-        return response([
-            "status" => 201,
-            "message" => "External books searched",
-            "data" => []
-        ]);
+        $book = Book::findOrFail($id);
+        
+        $book->name = $request->input('name');
+        $book->isbn = $request->input('isbn'); 
+        $book->authors = $request->input('authors');
+        $book->country = $request->input('country');
+        $book->number_of_pages = $request->input('number_of_pages');
+        $book->publisher = $request->input('publisher');
+        $book->release_date = $request->input('release_date');  
+        
+
+        if ($book->save()) {
+            $book->message = "Books updated";
+            $book->status_code = 201;
+            return new BookResource($book);
+        } 
     }
 
     public function deleteBook(Request $request, $id){
-        $url = "https://www.anapioficeandfire.com/api/";
+        $delete_books = Book::where('id',$id)->delete();
+
         return response([
             "status" => 201,
-            "message" => "Books deleted",
+            "message" => "Book deleted",
         ]);
     }
 
     public function showBook(Request $request, $id){
-        $url = $this->url . '/' . $id;
+        $book_show = Book::where('id',$id)->get();
 
-        $res = $this->externalEndPoint($url)->original;
+        $output = [];
+        foreach($book_show as $d => $a){
+            $output[] = [
+                            'id' => $a->id,
+                            'name' => $a->name,
+                            'isbn' => $a->isbn,
+                            'authors' => $a->authors,
+                            'number_of_pages' => $a->numberOfPages,
+                            'publisher' => $a->publisher,
+                            'country' => $a->country,
+                            'released_date' => $a->released
+                        ];
+        };
 
         return response([
             "status" => 201,
-            "message" => "External books searched",
-            "data" => $res
+            "message" => "Book retrieved",
+            "data" => $output
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@login');
 
+Route::get('external-books/{name?}', 'demoTestController@searchBookWithParam');
 Route::get('books', 'demoTestController@getBook');
 Route::get('books/{id}', 'demoTestController@showBook');
+Route::post('books', 'demoTestController@createBook');
+Route::put('books/{id}', 'demoTestController@updateBook');
+Route::delete('books/{id}', 'demoTestController@deleteBook');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
